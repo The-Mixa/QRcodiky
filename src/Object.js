77d28e9fbb39cc63698289897bfbc3028c98e15d
= <misha.oleg548@gmail.com>
2025-03-27 13:06:14 +0300

    a

--- src/Object.js
+++ src/Object.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 import axios from 'axios';
 import WorkList from './WorkList';
 import WorkListReview from './WorkListReview';
